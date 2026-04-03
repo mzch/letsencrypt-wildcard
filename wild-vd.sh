b956d4f4f3c9f6afee0f3d341453625d3bf55d37
Koichi MATSUMOTO <mzch@me.com>
2026-04-04 00:23:08 +0900

    fix bug of wrong script name - clean-hook-vd.sh.

--- wild-vd.sh
+++ wild-vd.sh
@@ -16,17 +16,31 @@ fi
 which dns-hook-vd.sh > /dev/null
 if [[ $? -ne 0 ]] ; then
   DNS_HOOK="/etc/letsencrypt/dns-hook-vd.sh"
+  if [[ ! -x $DNS_HOOK ]] ; then
+    echo "Not found dns-hook-vd.sh"
+    exit 2
+  fi
 else
   DNS_HOOK="dns-hook-vd.sh"
 fi
+
 which clean-hook-vd.sh > /dev/null
 if [[ $? -ne 0 ]] ; then
   CLN_HOOK="/etc/letsencrypt/clean-hook-vd.sh"
+  if [[ ! -x $CLN_HOOK ]] ; then
+    echo "Not found clean-hook-vd.sh"
+    exit 2
+  fi
 else
-  CLN_HOOK="dns-hook-vd.sh"
+  CLN_HOOK="clean-hook-vd.sh"
 fi
 
-certbot certonly --manual \
+SUDO=""
+if [[ $UID -ne 0 ]] ; then
+  SUDO="sudo"
+fi
+
+$SUDO certbot certonly --manual \
 	--agree-tos --preferred-challenges dns "$OPTS" \
 	--manual-auth-hook "$DNS_HOOK"
 	--manual-cleanup-hook "$CLN_HOOK"
